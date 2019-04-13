ae2aa2b5590abf94784c0b95f7b55ca1f7c4d3f0
Kangaechu <kangae2@gmail.com>
2019-04-13 15:31:26 +0900

    run for python3

--- gcalcron.py
+++ gcalcron.py
@@ -1,48 +1,43 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-#    gcalcron v2.1
-#
-#    Copyright Fabrice Bernhard 2011-2013
-#    fabriceb@theodo.fr
-#    www.theodo.fr
+#    gcalcron
 
 """Command-line synchronisation application between Google Calendar and Linux's Crontab.
 Usage:
-  $ python gcalcron2.py
+  $ python gcalcron3.py
 """
+
 from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
 from __future__ import absolute_import
 
-# Google API
-from builtins import open
-from builtins import int
-from future import standard_library
-
-standard_library.install_aliases()
-from builtins import str
-from builtins import input
-from builtins import object
 import argparse
 import httplib2
 import os
 import sys
+import json
+import datetime
+import dateutil.parser
+import subprocess
+import re
+import logging
+
+# Google API
+from builtins import open
+from builtins import int
+from builtins import str
+from builtins import input
+from builtins import object
 from apiclient import discovery
 from oauth2client import file
 from oauth2client import client
 from oauth2client import tools
-
-# GCalCron
-import json
-import datetime
-import dateutil.parser
+from future import standard_library
 from dateutil.tz import gettz
-import time
-import subprocess
-import re
-import logging
+
+standard_library.install_aliases()
 
 logger = logging.getLogger(__name__)
 
@@ -67,8 +62,8 @@ class GCalAdapter(object):
     # <https://cloud.google.com/console#/project/395452703880/apiui>
     CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
 
-    def __init__(self, calendarId=None, flags=None):
-        self.calendarId = calendarId
+    def __init__(self, calendar_id=None, flags=None):
+        self.calendarId = calendar_id
         self.flags = flags
         self.service = None
 
@@ -84,13 +79,13 @@ class GCalAdapter(object):
                 # Add one or more of the following scopes. PLEASE ONLY ADD THE SCOPES YOU
                 # NEED. For more information on using scopes please see
                 # <https://developers.google.com/+/best-practices>.
-                FLOW = client.flow_from_clientsecrets(self.CLIENT_SECRETS,
+                flow = client.flow_from_clientsecrets(self.CLIENT_SECRETS,
                                                       scope=[
                                                           'https://www.googleapis.com/auth/calendar.readonly',
                                                       ],
                                                       message=tools.message_if_missing(self.CLIENT_SECRETS))
 
-                credentials = tools.run_flow(FLOW, storage, self.flags)
+                credentials = tools.run_flow(flow, storage, self.flags)
 
             # Create an httplib2.Http object to handle our HTTP requests and authorize it
             # with our good Credentials.
@@ -106,15 +101,19 @@ class GCalAdapter(object):
         Builds the Google Calendar query with default options set
 
         >>> g = GCalAdapter()
-        >>> g.get_query(datetime.datetime(2011, 6, 19, 14, 0), datetime.datetime(2011, 6, 26, 14, 0), datetime.datetime(2011, 6, 18, 14, 0))
-        {'orderBy': 'updated', 'showDeleted': True, 'calendarId': None, 'timeMin': '2011-06-19T14:00:00', 'updatedMin': '2011-06-18T14:00:00', 'timeMax': '2011-06-26T14:00:00', 'fields': 'items(description,end,id,start,status,summary,updated)', 'singleEvents': True, 'maxResults': 1000}
+        >>> g.get_query(datetime.datetime(2011, 6, 19, 14, 0),
+        datetime.datetime(2011, 6, 26, 14, 0),
+        datetime.datetime(2011, 6, 18, 14, 0))
+        {'orderBy': 'updated', 'showDeleted': True, 'calendarId': None, 'timeMin': '2011-06-19T14:00:00',
+        'updatedMin': '2011-06-18T14:00:00', 'timeMax': '2011-06-26T14:00:00',
+        'fields': 'items(description,end,id,start,status,summary,updated)', 'singleEvents': True, 'maxResults': 1000}
 
         @author Fabrice Bernhard
         @since 2011-06-19
         """
 
         logger.info('Setting up query: %s to %s modified after %s' % (
-        start_min.isoformat(), start_max.isoformat(), updated_min))
+            start_min.isoformat(), start_max.isoformat(), updated_min))
 
         query = {
             'calendarId': self.calendarId,
@@ -133,7 +132,7 @@ class GCalAdapter(object):
 
         return query
 
-    def queryApi(self, queries):
+    def query_api(self, queries):
         """Query the Google Calendar API."""
 
         logger.info('Submitting query')
@@ -141,13 +140,13 @@ class GCalAdapter(object):
         entries = []
         for query in queries:
             try:
-                pageToken = None
+                page_token = None
                 while True:
-                    query['pageToken'] = pageToken
-                    gCalEvents = self.get_service().events().list(**query).execute()
-                    entries += gCalEvents['items']
-                    pageToken = gCalEvents.get('nextPageToken')
-                    if not pageToken:
+                    query['pageToken'] = page_token
+                    g_cal_events = self.get_service().events().list(**query).execute()
+                    entries += g_cal_events['items']
+                    page_token = g_cal_events.get('nextPageToken')
+                    if not page_token:
                         break
             except client.AccessTokenRefreshError:
                 print("The credentials have been revoked or expired, please re-run"
@@ -177,7 +176,7 @@ class GCalAdapter(object):
         else:
             queries.append(self.get_query(sync_start, end))
 
-        return self.queryApi(queries)
+        return self.query_api(queries)
 
 
 class GCalCron(object):
@@ -193,8 +192,8 @@ class GCalCron(object):
     settings = None
     settings_file = os.getenv('HOME') + '/' + '.gcalcron2'
 
-    def __init__(self, gCalAdapter=None):
-        self.gCalAdapter = gCalAdapter
+    def __init__(self, g_cal_adapter=None):
+        self.gCalAdapter = g_cal_adapter
         self.load_settings()
 
     def load_settings(self):
@@ -202,23 +201,24 @@ class GCalCron(object):
             with open(self.settings_file) as f:
                 self.settings = json.load(f)
         except IOError:
-            calendarId = input(
-                'Calendar id (in the form of XXXXX....XXXX@group.calendar.google.com or for the main one just your Google email): ')
-            self.init_settings(calendarId)
+            calendar_id = input(
+                'Calendar id (in the form of XXXXX....XXXX@group.calendar.google.com or for the main one just your '
+                'Google email): ')
+            self.init_settings(calendar_id)
             self.save_settings()
 
     def save_settings(self):
         with open(self.settings_file, 'w') as f:
             json.dump(self.settings, f, indent=2)
 
-    def init_settings(self, calendarId):
+    def init_settings(self, calendar_id):
         self.settings = {
             "jobs": {},
-            "calendarId": calendarId,
+            "calendarId": calendar_id,
             "last_sync": None
         }
 
-    def getCalendarId(self):
+    def get_calendar_id(self):
         return self.settings["calendarId"]
 
     def clean_settings(self):
@@ -257,7 +257,7 @@ class GCalCron(object):
 
     def schedule_new_jobs(self, events):
         for event in events:
-            if not 'commands' in event:
+            if 'commands' not in event:
                 continue
 
             for command in event['commands']:
@@ -277,7 +277,7 @@ class GCalCron(object):
 
                 if job_id_match:
                     job_id = job_id_match.group(1)
-                logger.debug('identified job_id: ' + job_id)
+                    logger.debug('identified job_id: ' + job_id)
 
                 if event['uid'] in self.settings['jobs']:
                     self.settings['jobs'][event['uid']]['ids'].append(job_id)
@@ -287,7 +287,7 @@ class GCalCron(object):
                         'ids': [job_id, ]
                     }
 
-    def sync_gcal_to_cron(self, num_days=datetime.timedelta(days=7), verbose=True):
+    def sync_gcal_to_cron(self, num_days=datetime.timedelta(days=7)):
         """
         - fetches a list of commands through the GoogleCalendar adapter
         - schedules them for execution using the unix "at" command
@@ -304,13 +304,13 @@ class GCalCron(object):
 
         sync_start = datetime.datetime.now(gettz())
         events = self.gCalAdapter.get_events(sync_start, last_sync, num_days)
-        commandsList = parse_events(events)
+        commands_list = parse_events(events)
 
         # first unschedule all modified/deleted events
-        self.unschedule_old_jobs(commandsList)
+        self.unschedule_old_jobs(commands_list)
 
         # then reschedule all modified/new events
-        self.schedule_new_jobs(commandsList)
+        self.schedule_new_jobs(commands_list)
 
         # clean old jobs from the settings
         self.clean_settings()
@@ -323,11 +323,16 @@ def parse_commands(event_description, start_time, end_time):
     """
     Parses the description of a Google calendar event and returns a list of commands to execute
 
-    >>> parse_commands("echo 'Wake up!'\\n+10: echo 'Wake up, you are 10 minutes late!'", datetime.datetime(3011, 6, 19, 8, 30), datetime.datetime(3011, 6, 19, 9, 0))
-    [{'exec_time': datetime.datetime(3011, 6, 19, 8, 30), 'command': "echo 'Wake up!'"}, {'exec_time': datetime.datetime(3011, 6, 19, 8, 40), 'command': "echo 'Wake up, you are 10 minutes late!'"}]
+    >>> parse_commands("echo 'Wake up!'\\n+10: echo 'Wake up, you are 10 minutes late!'", 
+    datetime.datetime(3011, 6, 19, 8, 30), datetime.datetime(3011, 6, 19, 9, 0))
+    [{'exec_time': datetime.datetime(3011, 6, 19, 8, 30), 'command': "echo 'Wake up!'"},
+    {'exec_time': datetime.datetime(3011, 6, 19, 8, 40), 'command': "echo 'Wake up, you are 10 minutes late!'"}]
 
-    >>> parse_commands("Turn on lights\\nend -10: Dim lights\\nend: Turn off lights", datetime.datetime(3011, 6, 19, 18, 30), datetime.datetime(3011, 6, 19, 23, 0))
-    [{'exec_time': datetime.datetime(3011, 6, 19, 18, 30), 'command': 'Turn on lights'}, {'exec_time': datetime.datetime(3011, 6, 19, 22, 50), 'command': 'Dim lights'}, {'exec_time': datetime.datetime(3011, 6, 19, 23, 0), 'command': 'Turn off lights'}]
+    >>> parse_commands("Turn on lights\\nend -10: Dim lights\\nend: Turn off lights",
+    datetime.datetime(3011, 6, 19, 18, 30), datetime.datetime(3011, 6, 19, 23, 0))
+    [{'exec_time': datetime.datetime(3011, 6, 19, 18, 30), 'command': 'Turn on lights'},
+    {'exec_time': datetime.datetime(3011, 6, 19, 22, 50), 'command': 'Dim lights'},
+    {'exec_time': datetime.datetime(3011, 6, 19, 23, 0), 'command': 'Turn off lights'}]
 
 
     @author Fabrice Bernhard
@@ -339,7 +344,7 @@ def parse_commands(event_description, start_time, end_time):
         exec_time = start_time
         # Supported syntax for offset prefixes:
         #   '[+-]10: ', 'end:', 'end[+-]10:', 'end [+-]10:'
-        offset_match = re.compile('^(end)? ?([\+,-]\d+)?: (.*)').search(command)
+        offset_match = re.compile("^(end)? ?([+,-]\d+)?: (.*)").search(command)
         if offset_match:
             if offset_match.group(1):
                 exec_time = end_time
@@ -366,16 +371,24 @@ def parse_events(events):
     """
     Transforms the Google Calendar API results into a list of commands
 
-    >>> parse_events([{u'status': u'confirmed', u'updated': u'2013-12-22T19:49:13.750Z', u'end': {u'dateTime': u'2113-12-23T02:00:00+01:00'}, u'description': u'-60: start_heating.py\\n0: turn_music_on.py\\n+30: stop_heating.py', u'summary': u'Wakeup', u'start': {u'dateTime': u'2113-12-23T01:00:00+01:00'}, u'id': u'olbia2urfm1ns0h88v4u0d9a5g'}])
-    [{'commands': [{'exec_time': datetime.datetime(2113, 12, 23, 0, 0), 'command': u'start_heating.py'}, {'exec_time': datetime.datetime(2113, 12, 23, 1, 0), 'command': u'0: turn_music_on.py'}, {'exec_time': datetime.datetime(2113, 12, 23, 1, 30), 'command': u'stop_heating.py'}], 'uid': u'olbia2urfm1ns0h88v4u0d9a5g'}]
+    >>> parse_events([{u'status': u'confirmed', u'updated': u'2013-12-22T19:49:13.750Z', u'end': {u'dateTime':
+    u'2113-12-23T02:00:00+01:00'}, u'description': u'-60: start_heating.py\\n0: turn_music_on.py\\n+30:
+    stop_heating.py', u'summary': u'Wakeup', u'start': {u'dateTime': u'2113-12-23T01:00:00+01:00'},
+    u'id': u'olbia2urfm1ns0h88v4u0d9a5g'}]) [{'commands': [{'exec_time': datetime.datetime(2113, 12, 23, 0, 0),
+    'command': u'start_heating.py'}, {'exec_time': datetime.datetime(2113, 12, 23, 1, 0), 'command': u'0:
+    turn_music_on.py'}, {'exec_time': datetime.datetime(2113, 12, 23, 1, 30), 'command': u'stop_heating.py'}],
+    'uid': u'olbia2urfm1ns0h88v4u0d9a5g'}]
 
-    >>> parse_events([{u'status': u'cancelled', u'updated': u'2013-12-22T19:52:50.525Z', u'end': {u'dateTime': u'2013-12-23T02:00:00+01:00'}, u'description': u'-60: start_heating.py\\n0: turn_music_on.py\\n+30: stop_heating.py', u'summary': u'Wakeup', u'start': {u'dateTime': u'2013-12-23T01:00:00+01:00'}, u'id': u'olbia2urfm1ns0h88v4u0d9a5g'}])
+    >>> parse_events([{u'status': u'cancelled', u'updated': u'2013-12-22T19:52:50.525Z',
+     u'end': {u'dateTime': u'2013-12-23T02:00:00+01:00'},
+     u'description': u'-60: start_heating.py\\n0: turn_music_on.py\\n+30: stop_heating.py', u'summary': u'Wakeup',
+     u'start': {u'dateTime': u'2013-12-23T01:00:00+01:00'}, u'id': u'olbia2urfm1ns0h88v4u0d9a5g'}])
     [{'uid': u'olbia2urfm1ns0h88v4u0d9a5g'}]
 
     @author Fabrice Bernhard
     @since 2013-12-22
     """
-    commandsList = []
+    commands_list = []
     for event in events:
         start_time = dateutil.parser.parse(event['start']['dateTime']).replace(tzinfo=None)
         end_time = dateutil.parser.parse(event['end']['dateTime']).replace(tzinfo=None)
@@ -388,20 +401,20 @@ def parse_events(events):
                 'dateTime'] + ') ' + '=>' + event_description)
         if event['status'] == 'cancelled':
             logger.info("cancelled " + event['id'])
-            commandsList.append({
+            commands_list.append({
                 'uid': event['id']
             })
         elif event_description:
             commands = parse_commands(event_description, start_time, end_time)
             if commands:
-                commandsList.append({
+                commands_list.append({
                     'uid': event['id'],
                     'commands': commands
                 })
 
-    logger.debug(commandsList)
+    logger.debug(commands_list)
 
-    return commandsList
+    return commands_list
 
 
 def datetime_to_at(dt):
@@ -428,8 +441,8 @@ def main(argv):
 
     try:
         g = GCalCron()
-        gCalAdapter = GCalAdapter(g.getCalendarId(), flags)
-        g.gCalAdapter = gCalAdapter
+        g_cal_adapter = GCalAdapter(g.get_calendar_id(), flags)
+        g.gCalAdapter = g_cal_adapter
 
         if flags.reset:
             g.reset_settings()
